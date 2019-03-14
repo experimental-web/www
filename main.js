335700d2a88253f24f2b44d3f8ff5158c20d02ed
Thomas Wang <iothomaswang@gmail.com>
2019-03-13 22:57:07 -0400

    add web's age and remove product hunt

--- main.js
+++ main.js
@@ -1,5 +1,35 @@
+const iPad = /iPad/.test(navigator.userAgent) && !window.MSStream;
+
+if (iPad) {
+  document.getElementById("box").style.display = "none";
+  document.getElementById("iPadCard").style.display = "block";
+}
+
+if (window.matchMedia("(max-width: 650px)").matches) {
+  /* The viewport is at most 650 pixels wide */
+  document.getElementById("box").removeAttribute("data-tilt-glare");
+}
+
+function yearsToYearsMonthsDays(value) {
+  const totalDays = value * 365;
+  const years = Math.floor(totalDays / 365);
+  const months = Math.floor((totalDays - years * 365) / 30);
+  const days = Math.floor(totalDays - years * 365 - months * 30);
+  const result = years + " years, " + months + " months, " + days + " days";
+  return result;
+}
+
+function displayAge() {
+  const birthday = "1989-03-12";
+  const age = moment().diff(birthday, "days");
+  const webAge = yearsToYearsMonthsDays(age / 365);
+  document.getElementById("web-age").textContent = webAge;
+}
+
 window.onload = function() {
   console.log("I love the web!");
+
+  displayAge();
 
   const line1 = Snap.select("#line-1");
   const line2 = Snap.select("#line-2");
@@ -13,15 +43,3 @@ window.onload = function() {
   };
   to2();
 };
-
-const iPad = /iPad/.test(navigator.userAgent) && !window.MSStream;
-
-if (iPad) {
-  document.getElementById("box").style.display = "none";
-  document.getElementById("iPadCard").style.display = "block";
-}
-
-if (window.matchMedia("(max-width: 650px)").matches) {
-  /* The viewport is at most 650 pixels wide */
-  document.getElementById("box").removeAttribute("data-tilt-glare");
-}
